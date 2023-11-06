c2900cdde36aa19f65d67b56069f093ed74893a8
wndjs8225 <wndjs8225@naver.com>
2023-11-06 10:27:17 +0900

    feat : 업적API 추가

---
+++ FrontEnd/GreenStep/src/Api/achievementAPi.tsx
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+//기본 axios 인스턴스 생성
+const axiosInstance = axios.create({
+    baseURL: 'https://k9b303.p.ssafy.io/api',
+  });
+
+/** ACHIEVEMENT API */
+export const AchievementAPI = {
+    /** 업적 조회*/
+    getAchievementAxios: function (achieveType : number) {
+      return axiosInstance.request({
+        method: "GET",
+        url: `/${achieveType}`,
+      });
+    },
+  }
