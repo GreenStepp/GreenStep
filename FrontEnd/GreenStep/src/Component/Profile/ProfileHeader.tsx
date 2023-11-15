038cb880431c164896aa7c513a3da1bb48116d95
wndjs8225 <wndjs8225@naver.com>
2023-11-15 13:36:10 +0900

    style: profile header 스타일 변경

--- FrontEnd/GreenStep/src/Component/Profile/ProfileHeader.tsx
+++ FrontEnd/GreenStep/src/Component/Profile/ProfileHeader.tsx
@@ -2,31 +2,23 @@ import { View, Text, Image, StyleSheet } from "react-native";
 import React from 'react';
 import Box from "../../Style/Box";
 import Hello from '../../Image/Profile/hello.png'
-import ImageStyle from "../../Style/Image";
 const ProfileHeader = ({name}:any) => {
 
     return(
         <View style={[Box.flexRowBox, {alignItems:'center'}]}>
             <Image source={Hello} style={[{width: 180,height:70, resizeMode:'contain'}]}></Image>
-            {/* <Text style={{fontSize:40, paddingLeft: 5, marginBottom:5}}>{name}</Text> */}
-            <Text style={styles.profileHeaderText}>Hello! {name}</Text>
-
+            <Text style={styles.profileHeaderText}>{name}</Text>
         </View>
     )
-  return(
-    <View>
-      <Text style={styles.profileHeaderText}>Hello! {name}</Text>
-    </View>
-  )
 }
 
 const styles = StyleSheet.create({
   profileHeaderText: {
-    paddingLeft: 20,
-    fontSize: 30,
+    paddingLeft: 5,
+    fontSize: 45,
     fontWeight: 'bold',
-    marginTop: 30,
-    marginBottom: 30,
+    marginTop: 25,
+    marginBottom: 40,
   }
 })
 
